5e9079cdb82670988318f0f29f8afaa1f09fb3a8
Michal Wajer <mikail94@gmail.com>
2018-04-19 12:22:08 +0200

    feat: Translates cube on right click and mouse move Adds some relaxation to rotating and translating a cube Improves scrolling a cube

--- js/app_cube.js
+++ js/app_cube.js
@@ -1,17 +1,29 @@
+let scene;
 let cube;
-let rotationX = 45;
-let rotationY = 45;
+let rotationX=45;
+let rotationY=45;
+let translationX = 0;
+let translationY = 0;
 let lastPos = {};
 let sides = [];
-let cond=false;
+let ifRotate=false;
+let ifTranslate = false;
 let scaleCube = 1;
+let moveX;
+let moveY;
 function init () {
   cube = document.querySelector(".cube-container");
-  let side;
+  scene = document.querySelector(".scene");
   for (let i=1;i<7;i++) {
     sides.push(document.querySelector(".side-"+i));
   }
-  document.querySelector("#start").onclick = animate;
+  let startRotationButton = document.querySelector("#start");
+  startRotationButton.addEventListener('mousedown', (e) => e.stopPropagation());
+  startRotationButton.addEventListener('mousedown', (e) => {
+    animate();
+    e.target.setAttribute('disabled', 'true');
+  });
+  document.body.addEventListener('contextmenu', startTranslatingCubeByMouse);
   document.body.addEventListener('mousemove', moveCubeByMouse);
   document.body.addEventListener('mousedown', startMovingByMouse);
   document.body.addEventListener('touchstart', startMovingByMouse);
@@ -25,15 +37,16 @@ function init () {
   document.body.addEventListener('mouseup', stopMovingByMouse);
   window.addEventListener('wheel', changePerspective);
 }
+function startTranslatingCubeByMouse (e) {
+  e.preventDefault();
+}
 function animate () {
   rotationX += 1;
   cube.style.transform = `rotateX(${rotationX}deg) rotateY(${rotationY}deg)`;
   window.requestAnimationFrame(animate);
 }
 function moveCubeByMouse (e) {
-  if(cond) {
-    let moveX;
-    let moveY;
+  if (ifRotate || ifTranslate) {
     if(e.touches) {
       moveX = e.touches[0].clientX-lastPos.clientX;
       lastPos.clientX = e.touches[0].clientX;
@@ -45,23 +58,77 @@ function moveCubeByMouse (e) {
       moveY = e.clientY-lastPos.clientY;
       lastPos.clientY = e.clientY;
     }
-    rotationY+=calculateDegreesY(moveX);
-    rotationX+=calculateDegreesX(moveY);
-    cube.style.transform = `rotateX(${rotationX}deg) rotateY(${rotationY}deg)`;
+    if (ifRotate) {
+      rotationY+=calculateDegreesY(moveX);
+      rotationX+=calculateDegreesX(moveY);
+      cube.style.transform = `rotateX(${rotationX}deg) rotateY(${rotationY}deg)`;
+    } else if (ifTranslate) {
+      translationX +=calculateTranslation(moveX);
+      translationY +=calculateTranslation(moveY);
+      scene.style.transform = `translateX(${translationX}px) translateY(${translationY}px)`;
+    }
   }
+}
+function calculateTranslation(diff) {
+  let scaling = 1;
+  return diff/scaling;
 }
 function startMovingByMouse (e) {
   if(e.touches) {
     lastPos.clientX = e.touches[0].clientX;
     lastPos.clientY = e.touches[0].clientY;
-  } else {
+    ifRotate=true;
+  } else
+  {
     lastPos.clientX = e.clientX;
     lastPos.clientY = e.clientY;
+    if (e.which === 1) {
+      ifRotate=true;
+    } else if(e.which === 3) {
+      ifTranslate=true;
+    }
   }
-  cond=true;
 }
 function stopMovingByMouse (e) {
-  cond=false;
+  if (ifRotate) {
+    ifRotate=false;
+    decayCubeRotating();
+  }
+
+  if (ifTranslate) {
+    ifTranslate=false;
+    decayCubeTranslating();
+  }
+}
+async function decayCubeRotating () {
+  for(let i=0; i< 20; i++) {
+    await rotateCubeAutomatically();
+    moveX=moveX/1.1;
+    moveY=moveY/1.1;
+  }
+}
+async function decayCubeTranslating () {
+  for(let i=0; i< 20; i++) {
+    await translateCubeAutomatically();
+    moveX=moveX/1.1;
+    moveY=moveY/1.1;
+  }
+}
+function translateCubeAutomatically () {
+  return new Promise((resolve, reject) => {
+    translationX +=calculateTranslation(moveX);
+    translationY +=calculateTranslation(moveY);
+    scene.style.transform = `translateX(${translationX}px) translateY(${translationY}px)`;
+    setTimeout(() => {resolve();},10);
+  })
+}
+function rotateCubeAutomatically () {
+  return new Promise((resolve, reject) => {
+    rotationY+=calculateDegreesY(moveX);
+    rotationX+=calculateDegreesX(moveY);
+    cube.style.transform = `rotateX(${rotationX}deg) rotateY(${rotationY}deg)`;
+    setTimeout(() => {resolve();},10);
+  })
 }
 function calculateDegreesX(diff) {
   let scaling = 0.01;
@@ -79,35 +146,34 @@ function changePerspective (e) {
   }
 }
 function moveCubeFarer () {
-  if(scaleCube <= 0.05 && scaleCube >0.01) {
-    scaleCube-=0.001;
-  } else if (scaleCube <=0.01) {
+  if (scaleCube <= 0.01) {
     scaleCube = 0.01;
+  } else if (scaleCube <=0.05 && scaleCube > 0.01) {
+    scaleCube-=0.001;
+  } else if (scaleCube <= 0.25 && scaleCube > 0.05) {
+    scaleCube-=0.005;
+  } else if (scaleCube <=0.5 && scaleCube > 0.25) {
+    scaleCube-=0.01;
   } else {
     scaleCube -=0.05;
   }
-
-  let height = sides[0].offsetWidth;
-  let translate = height*scaleCube/(2);
-  let side_1 = "rotateX(0deg) " + `translateZ(${translate}px) ` + `scale3d(${scaleCube},${scaleCube},${scaleCube})`;
-  let side_2 = "rotateY(90deg) " + `translateZ(${translate}px) ` + `scale3d(${scaleCube},${scaleCube},${scaleCube})`;
-  let side_3 = "rotateX(180deg) " + `translateZ(${translate}px) ` + `scale3d(${scaleCube},${scaleCube},${scaleCube})`;
-  let side_4 = "rotateY(-90deg) " + `translateZ(${translate}px) ` + `scale3d(${scaleCube},${scaleCube},${scaleCube})`;
-  let side_5 = "rotateX(90deg) " + `translateZ(${translate}px) ` + `scale3d(${scaleCube},${scaleCube},${scaleCube})`;
-  let side_6 = "rotateX(-90deg) " + `translateZ(${translate}px) ` + `scale3d(${scaleCube},${scaleCube},${scaleCube})`;
-  sides[0].style.transform = side_1;
-  sides[1].style.transform = side_2;
-  sides[2].style.transform = side_3;
-  sides[3].style.transform = side_4;
-  sides[4].style.transform = side_5;
-  sides[5].style.transform = side_6;
+  updateCube();
 }
-function moveCubeNearer() {
+function moveCubeNearer () {
   if (scaleCube < 0.049) {
     scaleCube+=0.001;
+  } else if (scaleCube < 0.0249) {
+    scaleCube+=0.005;
+  } else if (scaleCube < 0.49) {
+    scaleCube+=0.01;
+  } else if (scaleCube >= 4) {
+    scaleCube=scaleCube;
   } else {
     scaleCube +=0.05;
   }
+  updateCube();
+}
+function updateCube () {
   let height = sides[0].offsetWidth;
   let translate = height*scaleCube/(2);
   let side_1 = "rotateX(0deg) " + `translateZ(${translate}px) ` + `scale3d(${scaleCube},${scaleCube},${scaleCube})`;
